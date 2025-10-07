71d66e9989117b477ffaa859966d9388dd707d47
Dunzoai <marc@dunzoai.com>
2025-10-06 23:13:12 -0400

    Add Supabase images to Next.js remote patterns

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'njxydstcfczirnpskvad.supabase.co',
+        pathname: '/storage/v1/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
